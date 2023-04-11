ee65782663b2bf2ad543ac10eeceb9e906689191
Elvira-91 <elviramjeshtri@yahoo.com>
2023-04-11 12:23:24 +0200

    Initial commit for fullstack application

---
+++ src/main/java/com/elvira/customer/CustomerDao.java
@@ -0,0 +1,9 @@
+package com.elvira.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+    List<Customer> selectAllCustomers();
+    Optional<Customer> selectCustomerById(Integer id);
+}
---
+++ src/main/java/com/elvira/customer/CustomerService.java
@@ -0,0 +1,27 @@
+package com.elvira.customer;
+
+import com.elvira.exeption.ResourceNotFound;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerDao.selectAllCustomers();
+    }
+
+    public Customer getCustomer(Integer id) {
+        return customerDao.selectCustomerById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFound("customer with id [%s] not found".formatted(id))
+                );
+    }
+}
