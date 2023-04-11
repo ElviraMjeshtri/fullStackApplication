a689f22f977ed79664c4fc14e1c848b2fb0b5c36
Elvira-91 <elviramjeshtri@yahoo.com>
2023-04-11 15:40:39 +0200

    backend|Add JPA repository to the project

--- src/main/java/com/elvira/customer/CustomerService.java
+++ src/main/java/com/elvira/customer/CustomerService.java
@@ -1,6 +1,7 @@
 package com.elvira.customer;
 
 import com.elvira.exeption.ResourceNotFound;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,7 +11,7 @@ public class CustomerService {
 
     private final CustomerDao customerDao;
 
-    public CustomerService(CustomerDao customerDao) {
+    public CustomerService(@Qualifier("jpa") CustomerDao customerDao) {
         this.customerDao = customerDao;
     }
 
