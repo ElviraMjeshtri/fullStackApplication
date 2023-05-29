59bbb05dd717e6cf95af9b41bd054fc879e6a3ce
Elvira-91 <elviramjeshtri@yahoo.com>
2023-05-29 16:59:10 +0200

    frontend | TEST CD workflow

--- frontend/react/src/components/shared/sidebar.jsx
+++ frontend/react/src/components/shared/sidebar.jsx
@@ -113,7 +113,7 @@ const NavItem = ({icon, children, ...rest}) => {
                 role="group"
                 cursor="pointer"
                 _hover={{
-                    bg: 'blue.400',
+                    bg: 'red.400',
                     color: 'white',
                 }}
                 {...rest}>
