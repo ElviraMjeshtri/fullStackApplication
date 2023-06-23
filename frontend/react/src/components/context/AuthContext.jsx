a6ba53778b85979237e4901b3ebddcfca1941b04
Elvira-91 <elviramjeshtri@yahoo.com>
2023-06-23 11:44:32 +0200

    Login - React

---
+++ frontend/react/src/components/context/AuthContext.jsx
@@ -0,0 +1,75 @@
+import {
+    createContext,
+    useContext,
+    useEffect,
+    useState
+} from "react";
+import {login as performLogin} from "../../services/client.js";
+import jwtDecode from "jwt-decode";
+
+const AuthContext = createContext({})
+const AuthProvider = ({children}) => {
+    const [customer, setCustomer] = useState(null);
+    useEffect( ()=>{
+        let jwtToken = localStorage.getItem("access_token");
+        if (jwtToken){
+            jwtToken = jwtDecode(jwtToken);
+            setCustomer({
+                username: jwtToken.sub,
+                roles: jwtToken.scopes
+            });
+        }
+    }, [])
+    const login = async (usernameAndPassword) => {
+        return new Promise((resolve, reject) => {
+            performLogin(usernameAndPassword).then(res => {
+                const jwtToken = res.headers["authorization"]
+                localStorage.setItem("access_token", jwtToken);
+                const decodeJwtToken = jwtDecode(jwtToken);
+                setCustomer({
+                    username: decodeJwtToken.sub,
+                    roles: decodeJwtToken.scopes
+                })
+                resolve(res);
+            }).catch(err => {
+
+
+                reject(err)
+            })
+        })
+    }
+
+    const logOut = () => {
+        localStorage.removeItem("access_token");
+        setCustomer(null);
+    }
+    const isCustomerAuthenticated = () => {
+        const token = localStorage.getItem("access_token");
+        if (!token) {
+            return false;
+        }
+        const {exp: expiration} = jwtDecode(token);
+        if (Date.now() > expiration * 1000) {
+            logOut();
+            return false;
+        }
+        return true;
+    }
+
+    return (
+        <AuthContext.Provider value={
+            {
+                customer,
+                login,
+                logOut,
+                isCustomerAuthenticated
+            }
+        }>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(AuthContext)
+export default AuthProvider;
+
